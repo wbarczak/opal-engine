741e960aa3b28f71805b8c0e3bdb14f2424ab4b3
Barczak Wiktor <wbarczak67@gmail.com>
2025-11-27 13:30:33 +0100

    now sparse set should actually compile, and it can be used in a range based for loop

--- src/SparseSet.hpp
+++ src/SparseSet.hpp
@@ -3,92 +3,149 @@
 #include <vector>
 #include <limits>
 #include <algorithm>
-#include <utils>
+#include <utility>
 #include <cassert>
 
 template <typename T, size_t SIZE>
 class SparseSet
 {
 public:
-	SparseSet() : m_Sparse(SIZE, k_Empty), m_Indexes(SIZE, k_Empty)
+	SparseSet() : m_Sparse(SIZE, k_Empty)
 	{
 		m_Dense.reserve(SIZE);
+		m_Data.reserve(SIZE);
 	}
 
-	void push(size_t index, const T& item)
+	void insert(size_t index, const T& item)
 	{
-		assert(index < SIZE, "Trying to push to an index that's greater than the sparse set size!");
-		assert(m_Sparse[index] != k_Empty, "Trying to push a new item into an already occupied spot in a sparse set!");
+		assert(index < SIZE);
+		assert(m_Sparse[index] == k_Empty);
 
-		m_Dense.push_back(item);
-		m_Indexes.push_back(index);
-		m_Sparse[index] = m_Dense.size() - 1;
+		m_Data.push_back(item);
+		m_Dense.push_back(index);
+		m_Sparse[index] = m_Data.size() - 1;
 	}
 
 	template <typename ... Args>
 	void emplace(size_t index, Args&&... args)
 	{
-		assert(index < SIZE, "Trying to emplace to an index that's greater than the sparse set size!");
-		assert(m_Sparse[index] != k_Empty, "Trying to emplace a new item into an already occupied spot in a sparse set!");
+		assert(index < SIZE);
+		assert(m_Sparse[index] == k_Empty);
 
-		m_Dense.emplace(args);
-		m_Indexes.push_back(index);
-		m_Sparse[index] = m_Dense.size() - 1;
+		m_Data.emplace_back(std::forward<Args>(args)...);
+		m_Dense.push_back(index);
+		m_Sparse[index] = m_Data.size() - 1;
 	}
 
-	bool exists(size_t index) const
+	bool contains(size_t index) const
 	{
 		return m_Sparse[index] != k_Empty;
 	}
 
 	void pop(size_t index)
 	{
+		assert(index < SIZE);
+
 		const size_t denseIndex = m_Sparse[index];
 
-		if (denseIndex == m_Dense.size() - 1)
+		if (denseIndex == m_Data.size() - 1)
 		{
 			m_Sparse[index] = k_Empty;
 			m_Dense.pop_back();
+			m_Data.pop_back();
 			return;
 		}
 
-		const lastDenseIndex = m_Dense.size() - 1;
-		const lastSparseIndex = m_Indexes[lastDenseIndex];
+		const size_t lastDenseIndex = m_Data.size() - 1;
+		const size_t lastSparseIndex = m_Dense[lastDenseIndex];
 
-		std::swap(m_Dense[denseIndex], m_Dense.back());
+		std::swap(m_Data[denseIndex], m_Data.back());
 
-		m_Indexes[denseIndex] = lastSparseIndex;
+		m_Dense[denseIndex] = lastSparseIndex;
 		m_Sparse[lastSparseIndex] = denseIndex;
 
+		m_Data.pop_back();
 		m_Dense.pop_back();
 		m_Sparse[index] = k_Empty;
 	}
 
+	bool popIfContains(size_t index)
+	{
+		if (!contains(index)) return false;
+
+		pop(index);
+		return true;
+	}
+
 	const T& at(size_t index) const
 	{
-		assert(index < SIZE, "Looking up an index that's not in a sparse set!");
-		return m_Dense[m_Sparse[index]];
+		assert(index < SIZE);
+		return m_Data[m_Sparse[index]];
 	}
 
 	T& operator[](size_t index)
 	{
-		return m_Dense[m_Sparse[index]];
+		return m_Data[m_Sparse[index]];
 	}
 
 	bool empty() const
 	{
-		return m_Dense.empty();
+		return m_Data.empty();
 	}
 
 	void clear()
 	{
-		m_Sparse.assing(SIZE, k_Empty);
-		m_Indexes.assign(SIZE, k_Empty);
+		m_Sparse.assign(SIZE, k_Empty);
 		m_Dense.clear();
+		m_Data.clear();
 	}
+
+	class Iterator
+	{
+	public:
+
+		Iterator(SparseSet* set, size_t index) : m_Index(index), m_Set(set) {}
+
+		std::pair<size_t, T&> operator*() const
+		{
+			return { m_Set->m_Dense[m_Index], m_Set->m_Data[m_Index] };
+		}
+
+		Iterator& operator++()
+		{
+			++m_Index;
+			return *this;
+		}
+
+		bool operator==(const Iterator& o) const
+		{
+			return m_Index == o.m_Index && m_Set == o.m_Set;
+		}
+
+		bool operator!=(const Iterator& o) const
+		{
+			return !(*this == o);
+		}
+
+	private:
+
+		size_t m_Index;
+		SparseSet<T, SIZE>* m_Set;
+	};
+
+	Iterator begin()
+	{
+		return Iterator(this, 0);
+	}
+
+	Iterator end()
+	{
+		return Iterator(this, m_Data.size());
+	}
+
 private:
-	constexpr size_t k_Empty = std::numeric_limits<size_t>::max;
+	static constexpr size_t k_Empty = std::numeric_limits<size_t>::max();
 	std::vector<size_t> m_Sparse;
-	std::vector<size_t> m_Indexes;
-	std::vector<T> m_Dense;
+	std::vector<size_t> m_Dense;
+	std::vector<T> m_Data;
 };
